3f3b6bc462f37e6d35328f3b906109f040fd7ec5
IweZix <lucanclv2@gmail.com>
2024-07-18 23:07:22 +0200

    add: course 2

---
+++ Course 2/AmazingProductService/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO products(name, category, price) VALUES('MacBook Pro M1 Pro', 'Computer', 2899);
+INSERT INTO products(name, category, price) VALUES('MacBook', 'Computer', 1199);
+INSERT INTO products(name, category, price) VALUES('iPhone 15 Pro Max', 'Phone', 1499);
+INSERT INTO products(name, category, price) VALUES('iPhone 15', 'Phone', 999);
+INSERT INTO products(name, category, price) VALUES('iPad Pro', 'Tablet', 799);
+INSERT INTO products(name, category, price) VALUES('iPad', 'Tablet', 329);
+INSERT INTO products(name, category, price) VALUES('Apple Watch Series 7', 'Watch', 399);
+INSERT INTO products(name, category, price) VALUES('Apple Watch SE', 'Watch', 279);
+INSERT INTO products(name, category, price) VALUES('AirPods Pro', 'Headphones', 249);
